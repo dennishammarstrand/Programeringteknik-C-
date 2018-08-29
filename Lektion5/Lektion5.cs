59eff191b551481cd7f76e51962c0913e83ba3e0
dennishammarstrand <42570637+dennishammarstrand@users.noreply.github.com>
2018-08-29 14:50:05 +0200

    2018-08-29

---
+++ Lektion5/Lektion5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion5
+{
+    class Lektion5
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            //Exercise 1 Classified location
+            string inputText = Console.ReadLine();
+
+            if (inputText.StartsWith("Classified:"))
+            {
+                inputText = inputText.Replace("Area 51", "[AN UNDISCLOSED LOCATION]");
+                inputText = inputText.Replace("Classified:", "");
+            }
+            Console.WriteLine(inputText);
+            */
+
+            List<int> numbersSum = new List<int> { };
+
+            while (true)
+            {
+                    int number = int.Parse(Console.ReadLine());
+                    numbersSum.Add(number);
+            }
+
+
+        }
+    }
+}
---
+++ Lektion6/Lektion6.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion6
+{
+    class Lektion6
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            //Exercise 1 Repeat text
+            string text = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            string multiText = "";
+
+            for (int i = 0; i < number; i++)
+            {
+                multiText += text;
+            }
+            Console.WriteLine(multiText);
+            */
+
+            /*
+            //Exercise 2 Word reverse
+            string text = Console.ReadLine();
+            string reverse = "";
+
+            foreach (char t in text)
+            {
+                reverse = t + reverse;
+            }
+            Console.WriteLine(reverse);
+            */
+
+            /*
+            //Exercise 3 Password input correct
+            string password = Console.ReadLine();
+
+            if (password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Correct password input!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password input!");
+            }
+            */
+
+            
+            string text = Console.ReadLine();
+            string reverse = "";
+            string textTrim = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    textTrim += "";
+                }
+                else
+                {
+                    textTrim += text[i];
+                }
+            }
+            foreach (char t in textTrim)
+            {
+                reverse = t + reverse;
+            }
+            if (textTrim == reverse)
+            {
+                Console.WriteLine("It's a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("It's not a palindrome!");
+            }
+            
+        }
+    }
+}
